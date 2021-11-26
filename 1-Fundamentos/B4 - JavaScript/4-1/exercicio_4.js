fe6b114dfcbb3d24045948361189204a64c92b23
André Rodrigues <andre360.ars@gmail.com>
2021-11-26 16:22:42 -0300

    adiciona quarto exercicio do dia 4.1

---
+++ 1-Fundamentos/B4 - JavaScript/4-1/exercicio_4.js
@@ -0,0 +1,8 @@
+const grade = 59
+if(grade >= 80){
+    console.log("Parabéns, voce foi aprovado(a)")
+}else if(grade < 80 && grade >= 60){
+    console.log("Voce esta na lista de espera")
+}else{
+    console.log("voce foi reprovado(a)")  
+}  
