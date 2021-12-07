72fdb63ddbcd3329782f65ec2c1808e0c6004732
André Rodrigues <andre360.ars@gmail.com>
2021-12-07 20:56:40 -0300

    cria função que troca o texto das cartas ao mudar o texto da caixa de input

--- 1-Fundamentos/B5 - JavaScript - projetos/5-3/fixando/main.js
+++ 1-Fundamentos/B5 - JavaScript - projetos/5-3/fixando/main.js
@@ -5,16 +5,17 @@ const input = document.getElementById('input');
 const myWebpage = document.getElementById('my-spotrybefy');
 const container = document.querySelector(".container")
 let listsItens = document.querySelectorAll('li')
-let count = 0
+
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 function liClick (event) {
   for (let index = 0; index < listsItens.length; index++) {
-    if (listsItens[index].className === 'tech') {
+      if (listsItens[index].className === 'tech') {
         document.querySelectorAll('li')[index].className = '';
         break
     }    
@@ -27,6 +28,15 @@ for (let i = 0; i < listsItens.length; i += 1) {
 }
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+function changer() {
+    let tech = document.querySelector('.tech');
+
+    tech.innerText = input.value
+}
+
+input.addEventListener('change', changer)
+
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
