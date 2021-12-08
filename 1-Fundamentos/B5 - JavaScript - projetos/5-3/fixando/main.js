900de7f171ec1d0e9181bdb6f9f1064d8a7b43f8
André Rodrigues <andre360.ars@gmail.com>
2021-12-07 21:18:26 -0300

    cria função que ao clicar duas vezes no subtitulo vai pro meu portifólio

--- 1-Fundamentos/B5 - JavaScript - projetos/5-3/fixando/main.js
+++ 1-Fundamentos/B5 - JavaScript - projetos/5-3/fixando/main.js
@@ -41,7 +41,11 @@ input.addEventListener('change', changer)
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+function teleport() {
+    window.location.assign('https://andre-ars.github.io/','_blank');
+}
 
+myWebpage.addEventListener('dblclick', teleport)
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
 
