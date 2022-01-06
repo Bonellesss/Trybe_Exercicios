5dad8bafbf9d6820e22e70969f50f6885e7f03e4
André Rodrigues <andre360.ars@gmail.com>
2022-01-06 14:50:39 -0300

    atualiza script

--- 1-Fundamentos/B6 - HTML & CSS  avançado/6-2/Resumé-Part II/script.js
+++ 1-Fundamentos/B6 - HTML & CSS  avançado/6-2/Resumé-Part II/script.js
@@ -1,5 +1,8 @@
 let states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goías', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí','Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraíma', 'Santa Catarina', 'São Paulo','Sergipe', 'Tocantins'];
 let stateSelect = document.getElementById('state');
+let labels = ['Nome: ', 'E-mail: ', 'CPF: ', 'Endereço: ', 'Cidade: ', 'Estado: ', 'Tipo de Moradia: ', 'Resumo do currículo: ', 'Cargo: ', 'Descrição do Cargo: ', 'Data de Inicio: ']
+let nomes = ["name", "email", "cpf", "adress", "city", "state", "type", "abstract", "ocupation", "description", "date"]
+let form = document.getElementById('form');
 
 window.onload = function () {
     for (let i = 0;i < 27; i += 1) {
@@ -13,14 +16,27 @@ window.onload = function () {
 
 document.getElementById('submit').addEventListener('click', function (event)  {
     event.preventDefault();
-
-
+    createResume()
 })
 
-function createCurriculo() {
+function createResume() {
+    if (document.getElementsByTagName('main')[0].lastElementChild.id === 'div-resume') {
+    document.getElementById('div-resume').outerHTML = '';
+    }
     let container = document.createElement('div');
 
-    document.querySelector('main').appendChild(container);
+    container.id = 'div-resume';
+    document.getElementsByTagName('MAIN')[0].appendChild(container); 
+    
+    formPrint()
+}
 
-   
+function formPrint() {
+    let textContainer = document.getElementById('div-resume');
+    for (let i = 0; i < nomes.length; i += 1) {
+        let text = document.createElement('p');
+        let nome = nomes[i]
+        text.innerText = labels[i] + document.getElementById('form').nome.value;
+        textContainer.appendChild(text)
+    }
 }
