5a759212ef98165027dfd6b53cf5eb4f52dfcbac
André Rodrigues <andre360.ars@gmail.com>
2022-01-18 18:51:23 -0300

    cria função de sorteio, que verifica se a pessoa acertou o numero sorteado

--- 1-Fundamentos/B8 - Higher Order Functions/8-1/exercicios/raffle.js
+++ 1-Fundamentos/B8 - Higher Order Functions/8-1/exercicios/raffle.js
@@ -1,1 +1,15 @@
-const raffle = (callBack) => 
+const raffle = (maxNumber, myNumber, callBack) => {
+    const randomNumber = Math.floor(Math.random() * maxNumber + 1)   
+
+    return  `O número sorteado foi: ${randomNumber}, seu numero é: ${myNumber}, ${callBack(myNumber, randomNumber)}`
+}
+
+const numCheck = (number, raffleNum) => {
+    if (number === raffleNum) {
+        return "Parabéns você ganhou"
+    }
+
+    return "Tente novamente"
+}
+
+console.log(raffle(5, 5, numCheck)); 
