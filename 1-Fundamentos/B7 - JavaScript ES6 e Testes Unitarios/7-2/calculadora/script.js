6f133c5d88b0fc9e4f50bc39aaaf4e34e9b7cd0d
André Rodrigues <andre360.ars@gmail.com>
2022-01-18 15:18:58 -0300

    jf

--- 1-Fundamentos/B7 - JavaScript ES6 e Testes Unitarios/7-2/calculadora/script.js
+++ 1-Fundamentos/B7 - JavaScript ES6 e Testes Unitarios/7-2/calculadora/script.js
@@ -2,11 +2,13 @@
         const value1 = document.getElementById('value1').value;
         const value2 = document.getElementById('value2').value;
         const result = parseInt(value1) + parseInt(value2);
+       
         try {
+            isNum(value1, value2)
             isComplet(value1, value2);
             document.getElementById('result').innerHTML = `Resultado: ${result}`;
         } catch (error) {
-            document.getElementById('result').innerHTML = `<span style="color: red;">${error.message}</span>`      
+            document.getElementById('result').innerHTML = `<span style="color: red;">${error.message}</span>`;      
         } finally {
             document.getElementById('value1').value = '';
             document.getElementById('value2').value = '';
@@ -14,9 +16,16 @@
     }
 
     const isComplet = (value1 ,value2) => {         
+        if (isNaN(value1) || isNaN(value2)) {
+            throw new Error('Entrada Invalida! Os dois valorem devem ser numeros!') 
+        }
+
         if (value1 === '' || value2 === '') {
             throw new Error('Entrada incompleta! Adicione os dois valores antes de somar!')
         }
+    }
+
+    const isNum = (val1, val2) => {
     }
 
     window.onload = () => {
