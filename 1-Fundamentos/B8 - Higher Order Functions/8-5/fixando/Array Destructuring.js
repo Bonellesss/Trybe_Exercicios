bcde3fa6d5989abfdef18dbd125a7ca87a2af42b
André Rodrigues <andre360.ars@gmail.com>
2022-01-24 18:37:01 -0300

    adiciona exercicios de fixação

---
+++ 1-Fundamentos/B8 - Higher Order Functions/8-5/fixando/Array Destructuring.js
@@ -0,0 +1,19 @@
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+
+const [saud1, saud2] = saudacoes;
+
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+const array = [comida, animal, bebida]
+
+const [pet, drink, food] = array;
+
+console.log(pet);
+
+
