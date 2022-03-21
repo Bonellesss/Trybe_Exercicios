07378f8f4584343105c89aa4b824e678d1f182bf
Andre <andre360.ars@gmail.com>
2022-03-21 18:39:28 -0300

    add

--- 2-Front End/Bloco14_RTL/14.2-Mocks/jokes-mock/src/services/service.js
+++
@@ -1,3 +0,0 @@
-export default randomNumbers = () => {
-  const random = Math.floor(Math.random() * 101);
-}
--- 2-Front End/Bloco14_RTL/14.2-Mocks/jokes-mock/src/services/service.test.js
+++
@@ -1,8 +0,0 @@
-import randomNumbers from './service'
-
-describe('Testa funções', () => {
-  it('testa retorno da função randomNumbers', () => {
-    
-  });
-  
-});
