27fec16511c08d85fcdc372f373c50b039363cb7
Andre <andre360.ars@gmail.com>
2022-06-27 11:27:05 -0300

    adiciona exercicios de fixação do dia 23.2

---
+++ 3-Back End/23-layers/23-2/fixando/hello-msc/index.js
@@ -0,0 +1,25 @@
+// index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const Author = require('./controllers/Authors');
+const errorMiddleware = require('./middlewares/error');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/authors', Author.getAll);
+
+app.get('/authors/:id', Author.findById);
+
+app.post('/authors', Author.createAuthor);
+
+app.use(errorMiddleware);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
