5600501a92762c73cb20e58cf628e9f4d04bbaf3
Andre <andre360.ars@gmail.com>
2022-07-07 18:35:43 -0300

    adiciona exercicios de fixação

---
+++ 3-Back End/24-ORM/24-1/fixando/app-with-sequelize/seeders/20220707150126-users.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          full_name: "Leonardo",
+          email: "leo@test.com",
+          // com a mudança no nome das colunas, precisamos colocar no seed o formato correspondente a este novo nome
+          created_at: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updated_at: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        {
+          full_name: "JEduardo",
+          email: "edu@test.com",
+          created_at: Sequelize.literal("CURRENT_TIMESTAMP"),
+          updated_at: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+      ],
+      {}
+    ),
+
+  down: async (queryInterface) => queryInterface.bulkDelete("Users", null, {}),
+};
